99bf5b1bb824f49e872fa899d5fdebc33bd1c87b
Harsh Singh <kmrharsh50@gmail.com>
2024-10-03 21:41:56 +0530

    change

--- LibraryManagement/Migrations/20241003160824_Mig2.Designer.cs
+++ LibraryManagement/Migrations/20241003160824_Mig2.Designer.cs
@@ -4,6 +4,7 @@ using LibraryManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryManagement.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20241003160824_Mig2")]
+    partial class Mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LibraryManagement/Migrations/20241003160824_Mig2.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3,
+                column: "Author",
+                value: "Web Gentel");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3,
+                column: "Author",
+                value: "We Gentel");
+        }
+    }
+}
